463f56bd48d947c48fb1e2bff45c59178531f111
Zach Leatherman <zachleat@users.noreply.github.com>
2023-12-15 07:59:22 -0600

    Initial commit.

---
+++ README.md
@@ -0,0 +1,31 @@
+# `<snow-fall>`
+
+A web component to add snow to your web site (or to an element on your web site). Based off of [this Codepen](https://codepen.io/alphardex/pen/dyPorwJ) from [`alphardex`](https://codepen.io/alphardex).
+
+## Usage
+
+```html
+<script type="module" src="snow-fall.js"></script>
+
+<!-- Add snow to the page -->
+<snow-fall></snow-fall>
+
+<!-- Add snow to the first child element -->
+<snow-fall>
+	<div style="width: 300px; height: 300px">
+</snow-fall>
+```
+
+### Customization
+
+```html
+<!-- Change snow color -->
+<snow-fall style="--snow-fall-color: rebeccapurple"></snow-fall>
+
+<!-- Force mode -->
+<snow-fall mode="page"></snow-fall>
+<snow-fall mode="element"></snow-fall>
+
+<!-- Change number of snowflakes (default: 100) -->
+<snow-fall count="200"></snow-fall>
+```
---
+++ snow-fall.js
@@ -0,0 +1,111 @@
+class Snow extends HTMLElement {
+	static random(min, max) {
+		return min + Math.floor(Math.random() * (max - min) + 1);
+	}
+
+	static attrs = {
+		count: "count", // default: 100
+		mode: "mode",
+	}
+
+	generateCss(mode, count) {
+		let css = [];
+		css.push(`
+:host([mode="element"]) {
+	display: block;
+	position: relative;
+	overflow: hidden;
+}
+:host([mode="page"]) {
+	position: fixed;
+	top: 0;
+	left: 0;
+	right: 0;
+}
+* {
+	pointer-events: none;
+	position: absolute;
+	width: 10px;
+	height: 10px;
+	background: var(--snow-fall-color, rgba(255,255,255,.5));
+	border-radius: 50%;
+}
+`);
+
+		// using vw units (max 100)
+		let dimensions = { width: 100, height: 100 };
+		let units = { x: "vw", y: "vh"};
+
+		if(mode === "element") {
+			dimensions = {
+				width: this.firstElementChild.clientWidth,
+				height: this.firstElementChild.clientHeight,
+			};
+			units = { x: "px", y: "px"};
+		}
+
+		// Thank you @alphardex: https://codepen.io/alphardex/pen/dyPorwJ
+		for(let j = 1; j<= count; j++ ) {
+			let x = Snow.random(1, 90) * dimensions.width/100; // vw
+			let offset = Snow.random(-10, 10) * dimensions.width/100; // vw
+
+			let yoyo = Math.round(Snow.random(30, 100)); // % time
+			let yStart = yoyo * dimensions.height/100; // vh
+			let yEnd = dimensions.height; // vh
+
+			let scale = Snow.random(1, 10000) * .0001;
+			let duration = Snow.random(10, 30);
+			let delay = Snow.random(0, 30) * -1;
+
+			css.push(`
+:nth-child(${j}) {
+	opacity: ${Snow.random(0, 1000) * 0.001};
+	transform: translate(${x}${units.x}, -10px) scale(${scale});
+	animation: fall-${j} ${duration}s ${delay}s linear infinite;
+}
+
+@keyframes fall-${j} {
+	${yoyo}% {
+		transform: translate(${x + offset}${units.x}, ${yStart}${units.y}) scale(${scale});
+	}
+
+	to {
+		transform: translate(${x + offset / 2}${units.x}, ${yEnd}${units.y}) scale(${scale});
+	}
+}`)
+		}
+		return css.join("\n");
+	}
+
+	connectedCallback() {
+		// https://caniuse.com/mdn-api_cssstylesheet_replacesync
+		if(this.shadowRoot || !("replaceSync" in CSSStyleSheet.prototype)) {
+			return;
+		}
+
+		let count = parseInt(this.getAttribute(Snow.attrs.count)) || 100;
+
+		let mode;
+		if(this.hasAttribute(Snow.attrs.mode)) {
+			mode = this.getAttribute(Snow.attrs.mode);
+		} else {
+			mode = this.firstElementChild ? "element" : "page";
+			this.setAttribute(Snow.attrs.mode, mode);
+		}
+
+		let sheet = new CSSStyleSheet();
+		sheet.replaceSync(this.generateCss(mode, count));
+
+		let shadowroot = this.attachShadow({ mode: "open" });
+		shadowroot.adoptedStyleSheets = [sheet];
+
+		let d = document.createElement("div");
+		for(let j = 0, k = count; j<k; j++) {
+			shadowroot.appendChild(d.cloneNode());
+		}
+
+		shadowroot.appendChild(document.createElement("slot"));
+	}
+}
+
+customElements.define("snow-fall", Snow);
